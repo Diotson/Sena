0c33048bb08aaf321ab209457713f933e7a90a76
Diotson <93937096+Diotson@users.noreply.github.com>
2023-05-18 22:15:11 -0500

    Update lotus_glamping (1).sql

--- lotus_glamping (1).sql
+++ lotus_glamping (1).sql
@@ -1,309 +1,123 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 10-05-2023 a las 16:53:45
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
+Create database [Lotus_glamping]
+go
+use [Lotus Glamping]
+go
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+create table rol(
+ idrol int primary key not null,
+ nombre varchar(30) 
+)
+
+create table permisos(
+idpermisos int primary key not null,
+nombre varchar(30)
+)
+
+create table rol_permisos(
+idrol_permisos int primary key not null,
+nombre_operacion varchar(30),
+permiso int foreign key (permiso) references permisos (idpermisos),
+rol int foreign key (rol) references rol(idrol)
+)
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+create table administrador(
+idadministrador int primary key not null,
+nombres varchar (30),
+apellidos varchar (30),
+correo varchar (60),
+pass varchar (20),
+rol_permisos int foreign key (rol_permisos) references rol_permisos(idrol_permisos),
+)
 
---
--- Base de datos: `lotus_glamping`
---
+create table usuario(
+cedula int primary key not null,
+nombres varchar (30),
+apellidos varchar (30),
+correo varchar (60),
+pass varchar (20),
+celular varchar (15),
+rol_permisos int foreign key (rol_permisos) references rol_permisos(idrol_permisos),
+)
 
--- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `caracteristicas`
---
 
-CREATE TABLE `caracteristicas` (
-  `idcaracteristica` int(11) NOT NULL,
-  `nombre` varchar(30) DEFAULT NULL,
-  `detalle` varchar(100) DEFAULT NULL,
-  `cantidad` char(1) DEFAULT NULL,
-  `estado` char(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
+create table caracteristicas (
+idcaracteristica int primary key not null,
+nombre varchar(30),
+detalle varchar (100),
+cantidad char(1) ,
+estado char(10)
+)
 
---
--- Estructura de tabla para la tabla `domo`
---
 
-CREATE TABLE `domo` (
-  `Iddomo` int(11) NOT NULL,
-  `nombre` varchar(100) DEFAULT NULL,
-  `descripcion` varchar(100) DEFAULT NULL,
-  `disponibilidad` char(1) DEFAULT NULL,
-  `caracteristicas` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+create table servicios (
+idservicio int primary key not null,
+nombre varchar (60),
+precio float (10),
+estado nchar(1)
 
--- --------------------------------------------------------
+)
 
---
--- Estructura de tabla para la tabla `permisos`
---
+create table domo (
+Iddomo int primary key not null,
+nombre varchar (100),
+descripcion varchar (100),
+)
 
-CREATE TABLE `permisos` (
-  `idpermiso` int(11) NOT NULL,
-  `nombre_permiso` varchar(30) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+create table planes(
+idplan int  primary key not null,
+nombre varchar (80),
+descripcion varchar (100),
+precio_plandoble float (10),
+domo int foreign key (domo) references domo(iddomo),
+caracteristicas int foreign key (caracteristicas) references caracteristicas (idcaracteristica),
+servicios int foreign key (servicios) references servicios(idservicio)
+)
 
--- --------------------------------------------------------
+create table reserva(
+id_reserva int primary key not null,
+fecha_reserva date not null,
+fecha_inicio date not null,
+fecha_fin date not null,
+numero_personas int not null,
+recomendacion varchar(500),
+precio float (10),
+usuario int foreign key (usuario) references usuario(cedula),
+)
 
---
--- Estructura de tabla para la tabla `plan_paquete`
---
+create table servicios_reserva (
+idservicios_reserva int primary key not null,
+precio float (10),
+cantidad int,
+servicio int foreign key (servicio)references servicios(idservicio),
+reserva int foreign key (reserva) references reserva(id_reserva),
 
-CREATE TABLE `plan_paquete` (
-  `idplan` int(11) NOT NULL,
-  `nombre` varchar(80) DEFAULT NULL,
-  `descripcion` varchar(100) DEFAULT NULL,
-  `precio_plandoble` float DEFAULT NULL,
-  `caracteristicas` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+)
 
--- --------------------------------------------------------
+create table domo_reserva (
+Iddomo_reserva int primary key not null,
+precio float (10),
+cantidad int,
+domo int foreign key (domo) references domo(iddomo),
+reserva int foreign key (reserva) references reserva(id_reserva),
+)
 
---
--- Estructura de tabla para la tabla `recomendacion`
---
+create table planes_reserva(
+idplan_reserva int  primary key not null,
+precio float (10),
+reserva int foreign key (reserva) references reserva(id_reserva),
+planes int foreign key (planes) references planes (idplan),
+cantidad int
+)
 
-CREATE TABLE `recomendacion` (
-  `idrecomendacion` int(11) NOT NULL,
-  `cedula` int(11) DEFAULT NULL,
-  `descripcion` varchar(100) DEFAULT NULL,
-  `fecharegistro` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
+create table ventas (
+idventa int primary key not null,
+fecha_registro date, 
+total float(10),
+precio float(10),
+)
 
---
--- Estructura de tabla para la tabla `reserva_hospedaje`
---
 
-CREATE TABLE `reserva_hospedaje` (
-  `cod` int(11) NOT NULL,
-  `fecha_reserva` date NOT NULL,
-  `fecha_inicio` date NOT NULL,
-  `fecha_fin` date NOT NULL,
-  `numero_personas` int(11) NOT NULL,
-  `precio` float DEFAULT NULL,
-  `servicio` int(11) DEFAULT NULL,
-  `domo` int(11) DEFAULT NULL,
-  `paquete` int(11) DEFAULT NULL,
-  `usuario` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `rol`
---
-
-CREATE TABLE `rol` (
-  `idrol` int(11) NOT NULL,
-  `nombre` varchar(30) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `rol_permisos`
---
-
-CREATE TABLE `rol_permisos` (
-  `idrol_permisos` int(11) NOT NULL,
-  `idrol` int(11) DEFAULT NULL,
-  `idpermiso` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `servicios`
---
-
-CREATE TABLE `servicios` (
-  `idservicio` int(11) NOT NULL,
-  `nombre` varchar(60) DEFAULT NULL,
-  `precio` float DEFAULT NULL,
-  `estado` char(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuario`
---
-
-CREATE TABLE `usuario` (
-  `cedula` int(11) NOT NULL,
-  `nombres` varchar(30) DEFAULT NULL,
-  `apellidos` varchar(30) DEFAULT NULL,
-  `f_nacimiento` date DEFAULT NULL,
-  `direccion_residencia` varchar(40) DEFAULT NULL,
-  `correo` varchar(60) DEFAULT NULL,
-  `contraseña` varchar(20) DEFAULT NULL,
-  `idrol` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `ventas`
---
-
-CREATE TABLE `ventas` (
-  `idventa` int(11) NOT NULL,
-  `fecha_registro` date DEFAULT NULL,
-  `total` float DEFAULT NULL,
-  `cod` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `caracteristicas`
---
-ALTER TABLE `caracteristicas`
-  ADD PRIMARY KEY (`idcaracteristica`);
-
---
--- Indices de la tabla `domo`
---
-ALTER TABLE `domo`
-  ADD PRIMARY KEY (`Iddomo`),
-  ADD KEY `caracteristicas` (`caracteristicas`);
-
---
--- Indices de la tabla `permisos`
---
-ALTER TABLE `permisos`
-  ADD PRIMARY KEY (`idpermiso`);
-
---
--- Indices de la tabla `plan_paquete`
---
-ALTER TABLE `plan_paquete`
-  ADD PRIMARY KEY (`idplan`),
-  ADD KEY `caracteristica` (`caracteristicas`);
-
---
--- Indices de la tabla `recomendacion`
---
-ALTER TABLE `recomendacion`
-  ADD PRIMARY KEY (`idrecomendacion`),
-  ADD KEY `cedula` (`cedula`);
-
---
--- Indices de la tabla `reserva_hospedaje`
---
-ALTER TABLE `reserva_hospedaje`
-  ADD PRIMARY KEY (`cod`),
-  ADD KEY `servicio` (`servicio`),
-  ADD KEY `domo` (`domo`),
-  ADD KEY `paquete` (`paquete`),
-  ADD KEY `usuario` (`usuario`);
-
---
--- Indices de la tabla `rol`
---
-ALTER TABLE `rol`
-  ADD PRIMARY KEY (`idrol`);
-
---
--- Indices de la tabla `rol_permisos`
---
-ALTER TABLE `rol_permisos`
-  ADD PRIMARY KEY (`idrol_permisos`),
-  ADD KEY `idrol` (`idrol`),
-  ADD KEY `idpermiso` (`idpermiso`);
-
---
--- Indices de la tabla `servicios`
---
-ALTER TABLE `servicios`
-  ADD PRIMARY KEY (`idservicio`);
-
---
--- Indices de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`cedula`),
-  ADD KEY `rol` (`idrol`);
-
---
--- Indices de la tabla `ventas`
---
-ALTER TABLE `ventas`
-  ADD PRIMARY KEY (`idventa`),
-  ADD KEY `cod` (`cod`);
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `domo`
---
-ALTER TABLE `domo`
-  ADD CONSTRAINT `caracteristicas` FOREIGN KEY (`caracteristicas`) REFERENCES `caracteristicas` (`idcaracteristica`);
-
---
--- Filtros para la tabla `plan_paquete`
---
-ALTER TABLE `plan_paquete`
-  ADD CONSTRAINT `caracteristica` FOREIGN KEY (`caracteristicas`) REFERENCES `caracteristicas` (`idcaracteristica`);
-
---
--- Filtros para la tabla `recomendacion`
---
-ALTER TABLE `recomendacion`
-  ADD CONSTRAINT `cedula` FOREIGN KEY (`cedula`) REFERENCES `usuario` (`cedula`);
-
---
--- Filtros para la tabla `reserva_hospedaje`
---
-ALTER TABLE `reserva_hospedaje`
-  ADD CONSTRAINT `domo` FOREIGN KEY (`domo`) REFERENCES `domo` (`Iddomo`),
-  ADD CONSTRAINT `paquete` FOREIGN KEY (`paquete`) REFERENCES `plan_paquete` (`idplan`),
-  ADD CONSTRAINT `servicio` FOREIGN KEY (`servicio`) REFERENCES `servicios` (`idservicio`),
-  ADD CONSTRAINT `usuario` FOREIGN KEY (`usuario`) REFERENCES `usuario` (`cedula`);
-
---
--- Filtros para la tabla `rol_permisos`
---
-ALTER TABLE `rol_permisos`
-  ADD CONSTRAINT `idpermiso` FOREIGN KEY (`idpermiso`) REFERENCES `permisos` (`idpermiso`),
-  ADD CONSTRAINT `idrol` FOREIGN KEY (`idrol`) REFERENCES `rol` (`idrol`);
-
---
--- Filtros para la tabla `usuario`
---
-ALTER TABLE `usuario`
-  ADD CONSTRAINT `rol` FOREIGN KEY (`idrol`) REFERENCES `rol` (`idrol`);
-
---
--- Filtros para la tabla `ventas`
---
-ALTER TABLE `ventas`
-  ADD CONSTRAINT `cod` FOREIGN KEY (`cod`) REFERENCES `reserva_hospedaje` (`cod`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
